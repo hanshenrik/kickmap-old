20f7ed05d810cf51b46bf421b2575431b1d94ae5
Hans Henrik Grønsleth <henrik.gronsleth@knowit.no>
2016-11-25 00:21:44 +0100

    Shitloads

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+setupMap();
+
+getConcerts(31422);
+
+addScrollListener();
--- js/map.js
+++ js/map.js
@@ -1,10 +1,9 @@
 
 // Variables
-var center = [10.73, 59.92];
-var zoomLevel = 13;
-var timeAtEachConcert = 5000; // milliseconds
+var map;
 var activeConcertId;
-var colors = ['#51FFAE', '#17F08A', '#2DFC9B', '#00C86A']
+var timeAtEachConcert = 2000; // milliseconds
+var colors = ['#7DFFC2', '#17F08A', '#00C86A'];
 var highlightVenueGeoJson =
   {
     "type": "geojson",
@@ -17,51 +16,41 @@ var highlightVenueGeoJson =
         }
       }]
     }
-  }
+  };
 
 
-// Configure Mapbox
-mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+function setupMap() {
+  mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/dark-v9',
-  center: center,
-  zoom: zoomLevel
-});
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v9', // mapbox://styles/hanshenrik/civwyr9w800442klkf66ughei
+    center: [10.73, 59.92],
+    zoom: 13
+  });
+}
+
+
+function addSources() {
+  console.log('Adding concerts as a data source to the map');
+
+  // Add concerts as a source from our GeoJSON object
+  map.addSource("concerts", {
+    type: "geojson",
+    data: concertsCollection,
+    cluster: true,
+    clusterMaxZoom: 20, // Max zoom to cluster points on
+    clusterRadius: 30 // Radius of each cluster when clustering points (defaults to 50)
+  });
+}
 
 
 function addLayers() {
-  // Also, add a data source for all venues
-  map.addSource("venues", {
-    type: "geojson",
-    data: venuesGeoJson
-  });
+  console.log('Adding the sources as layers to the map');
 
-  // Add a layer showing the venues.
-  map.addLayer({
-    "id": "venues",
-    "type": "symbol",
-    "source": "venues",
-    "layout": {
-      "icon-image": "marker-15",
-      "icon-allow-overlap": true
-    }
-  });
-
-  // Add a new source from our GeoJSON data and set the
-  // 'cluster' option to true.
-  map.addSource("concerts", {
-    type: "geojson",
-    data: concertsGeoJson,
-    cluster: true,
-    clusterMaxZoom: 20, // Max zoom to cluster points on
-    clusterRadius: 20 // Radius of each cluster when clustering points (defaults to 50)
-  });
-
-  // Use the concerts source to create five layers:
+  // Use the concerts source to create a few layers:
   // - one for unclustered points,
-  // - three for each cluster category,
+  // - one for each cluster category so we can differentiate on how many are in the cluster,
   // - one for cluster labels
   map.addLayer({
     "id": "unclustered-points",
@@ -83,7 +72,7 @@ function addLayers() {
   // Display the concert data in three layers, each filtered to a range of
   // count values. Each range gets a different fill color.
   var layers = [
-      [50, colors[3]],
+      [50, colors[2]],
       [20, colors[1]],
       [0, colors[0]]
   ];
@@ -118,68 +107,25 @@ function addLayers() {
 }
 
 
-// When a click event occurs near a polygon, open a popup at the location of
-// the feature, with description HTML from its properties.
-map.on('click', function (e) {
-  var features = map.queryRenderedFeatures(e.point, { layers: ['unclustered-points', 'venues'] });
-  if (!features.length) {
-    return;
-  }
-
-  var feature = features[0];
-
-  var popup = new mapboxgl.Popup()
-    .setLngLat(map.unproject(e.point))
-    .setHTML(feature.properties.title)
-    .addTo(map);
-});
-
-
-// Loops through all concerts indefinitely
-function playback(index) {
-  // Index of next concert. Modulus length makes it 0 if we're at the last index
-  // var nextIndex = (index + 1) % markers.length;
-  var nextIndex = (index + 1) % concertsGeoJson.features.length;
-
-  // Get the current concert
-  // var marker = markers[index];
-  var concertFeature = concertsGeoJson.features[index];
-
-  // Show the info for this concert
-  // marker.togglePopup();
-
-  // Scroll to the correct info section
-  $("html, body").animate({ scrollTop: $('#concert-' + concertFeature.properties.id).position().top + 2 }, 800)
-
-  // Once the flight has ended, initiate a timeout that triggers a recursive call
-  map.once('moveend', function() {
-    window.setTimeout( function() {
-      // Hide the popup of this marker
-      // marker.togglePopup();
-
-      // Recursive call, fly to next concert
-      playback(nextIndex);
-    }, timeAtEachConcert); // After callback, stay at the location for x milliseconds
-  });
+function addScrollListener() {
+  window.onscroll = function() {
+    var concertIds = Object.keys(concertsCollection.features);
+    $.each(concertsCollection.features, function() {
+      var concertId = 'concert-' + this.properties.id;
+      if (isElementOnScreen(concertId)) {
+        setActiveConcert(this);
+        return false;
+      }
+    })
+  };
 }
 
 
-window.onscroll = function() {
-  var concertIds = Object.keys(concertsGeoJson.features);
-  $.each(concertsGeoJson.features, function() {
-    var concertId = 'concert-' + this.properties.id;
-    if (isElementOnScreen(concertId)) {
-      setActiveChapter(this);
-      return false;
-    }
-  })
-};
-
-function setActiveChapter(concertFeature) {
+function setActiveConcert(concertFeature) {
   var concertId = 'concert-' + concertFeature.properties.id;
   if (concertId === activeConcertId) return;
 
-  // Highlight the position on the map to feed the user with a spoon where the concert is
+  // Highlight the concert position on the map
   highlightVenueGeoJson.data.features[0].geometry.coordinates = concertFeature.geometry.coordinates;
 
   if (map.getSource('highlight-venue') !== undefined) {
@@ -195,28 +141,51 @@ function setActiveChapter(concertFeature) {
       "paint": {
         "circle-radius": 30,
         "circle-color": "lightyellow",
-        "circle-opacity": 0.4
+        "circle-opacity": 0.3
       }
     });
   }
 
+  // map.setCenter(concertFeature.geometry.coordinates);
+
   // Animate the map position based on camera properties
   map.flyTo({
     center: concertFeature.geometry.coordinates,
-    speed: 0.2,                        // Speed of the flight
+    speed: 0.5,                        // Speed of the flight
     curve: 1.3,                        // How far out it should zoom on the flight from A to B
     zoom: getRandomNumber(14, 17),     // Set a random zoom level for effect
     pitch: getRandomNumber(0, 61),     // Pitch for coolness
     bearing: getRandomNumber(-10, 10)  // Tilt north direction slightly for even more coolness
   });
 
-  // map.flyTo(chapters[concertId]);
-
   $('#' + concertId).addClass('active');
   $('#' + activeConcertId).removeClass('active');
 
   activeConcertId = concertId;
 }
+
+
+// Loop through all concerts indefinitely
+function playback(index) {
+  // Get the current concert
+  var concertFeature = concertsCollection.features[index];
+
+  // Scroll to the correct concert section
+  $('html, body').animate({ scrollTop: $('#concert-' + concertFeature.properties.id).position().top + 2 }, 800)
+
+  // Once the flight has ended, initiate a timeout that triggers a recursive call
+  map.once('moveend', function() {
+    window.setTimeout( function() {
+      // Get index of the next concert.
+      // Modulus length makes it 0 if we're at the last index, i.e. we'll start from the beginning again.
+      var nextIndex = (index + 1) % concertsCollection.features.length;
+
+      // Recursive call, fly to next concert
+      playback(nextIndex);
+    }, timeAtEachConcert); // After callback, stay at the location for x milliseconds
+  });
+}
+
 
 function isElementOnScreen(id) {
   var element = document.getElementById(id);
@@ -229,4 +198,3 @@ function isElementOnScreen(id) {
 function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
-
